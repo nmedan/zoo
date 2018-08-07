db069867eb69013c36db3fb86d34da91c5fdc262
nmedan <nmedan92@gmail.com>
2018-08-07 21:03:15 +0200

    Added sectors

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -7,18 +7,26 @@
             <input v-model="newAnimal.name" type="text" placeholder="Name"><br>
             <label>Date of birth: </label>
             <input v-model="newAnimal.birth" type="text" placeholder="Date of birth"><br>
+            <label>Sector:</label>
+            <select v-model="newAnimal.sector">
+                <option v-for="(sector, key) in sectors" :key="key" v-bind:value="sector">
+                    {{sector.name}}
+                </option>
+            </select><br>
             <button type="submit" @click="addAnimal">Add animal</button>
         </form>
         <table>
             <thead>
                 <th>Type</th>
                 <th>Name </th>
+                <th>Sector </th>
                 <th>Date of birth</th>
             </thead>
             <tbody>
                  <tr v-for="(animal, key) in animals" :key="key">                      
                       <td>{{animal.type}}</td>                     
                       <td>{{animal.name}}</td>
+                      <td>{{animal.sector.name}}</td>
                       <td>{{animal.birth? animal.birth : "Nepoznato"}}</td>
                       <td><button type="submit" @click="removeAnimal(animal)">Remove</button></td>
                       <td><button type="submit" @click="moveToTop(animal)">Move to top</button></td>
@@ -34,16 +42,22 @@ export default {
   data() {
       return {
           animals:[
-              {type: "Lion", name: "LionName", birth:""},
-              {type: "Wolf", name: "WolfName", birth:""},
-              {type: "Fox", name: "FoxName", birth: "22-08-2015"},
-              {type: "Rabbit", name: "RabbitName", birth: "24-10-2015"},
-              {type: "Bear", name: "BearName", birth: "22-09-2016"}
+              {type: "Lion", name: "LionName", birth:"", sector:{}},
+              {type: "Wolf", name: "WolfName", birth:"", sector:{}},
+              {type: "Fox", name: "FoxName", birth: "22-08-2015", sector:{}},
+              {type: "Rabbit", name: "RabbitName", birth: "24-10-2015", sector:{}},
+              {type: "Bear", name: "BearName", birth: "22-09-2016", sector:{}}
           ],
+          sectors:[
+              {name:"Sector1"},
+              {name:"Sector2"},
+              {name:"Sector3"},
+              {name:"Sector4"},
+              {name:"Sector5"}              
+          ],
+          
           newAnimal: {
-              type:'',
-              name:'',
-              birth:''
+              
           }
       };
     },
