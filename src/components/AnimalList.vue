4418fd6eef67d7cb32abb4e7642dd466e8dc94f3
nmedan <nmedan92@gmail.com>
2018-08-07 18:53:52 +0200

    Added 'Nepoznato' for empty day of birth

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -8,9 +8,10 @@
             </thead>
             <tbody>
                  <tr v-for="(animal, key) in animals" :key="key">                      
-                      <td>{{animal.type}}</td>
+                      <td>{{animal.type}}</td>                     
                       <td>{{animal.name}}</td>
-                      <td>{{animal.birth}}</td>   
+                      <td v-if="animal.birth === ''">Nepoznato</td>
+                      <td v-else>{{animal.birth}}</td>   
                  </tr>
             </tbody>
         </table>
@@ -23,8 +24,8 @@ export default {
   data() {
       return {
           animals:[
-              {type: "Lion", name: "LionName", birth: "24-09-2015"},
-              {type: "Wolf", name: "WolfName", birth: "24-09-2016"},
+              {type: "Lion", name: "LionName", birth:""},
+              {type: "Wolf", name: "WolfName", birth:""},
               {type: "Fox", name: "FoxName", birth: "22-08-2015"},
               {type: "Rabbit", name: "RabbitName", birth: "24-10-2015"},
               {type: "Bear", name: "BearName", birth: "22-09-2016"}
